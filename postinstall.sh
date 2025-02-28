9ef14117e3f02daeb7e8cbd1a59e7ac8ebf78197
Alvaro Lopez Medina <alvarolop.medina@gmail.com>
2025-02-28 16:48:32 +0100

    Include latest changes in the script

--- postinstall.sh
+++ postinstall.sh
@@ -50,6 +50,10 @@ oc apply -f 04-application-hashicorp-vault-server.yaml
 
 echo "Vault has been deployed and exposed in the 'vault' namespace."
 
+echo "Initializing Hashicorp Vault..."
+
+./04-create_vault_secrets.sh
+
 
 echo -e "\n============================"
 echo -e "=      INSTALL FREEIPA     ="
@@ -78,100 +82,6 @@ oc patch service freeipa -n ipa -p '{
 
 echo -e "\tConfiguring groups and users..."
 
-# Login to kerberos
-oc exec -it deployment/freeipa -n ipa -- \
-    sh -c "echo Passw0rd123 | /usr/bin/kinit admin && \
-    echo Passw0rd | \
-    ipa user-add ldap_admin --first=ldap \
-    --last=admin --email=ldap_admin@redhatlabs.dev --password"
-    
-# Create groups if they dont exist
-
-oc exec -it deployment/freeipa -n ipa -- \
-    sh -c "ipa group-add student --desc 'wrapper group' || true && \
-    ipa group-add ocp_admins --desc 'admin openshift group' || true && \
-    ipa group-add ocp_devs --desc 'edit openshift group' || true && \
-    ipa group-add ocp_viewers --desc 'view openshift group' || true && \
-    ipa group-add-member student --groups=ocp_admins --groups=ocp_devs --groups=ocp_viewers || true"
-
-# Add demo users
-
-oc exec -it deployment/freeipa -n ipa -- \
-    sh -c "echo Passw0rd | \
-    ipa user-add paul --first=paul \
-    --last=ipa --email=paulipa@redhatlabs.dev --password || true && \
-    ipa group-add-member ocp_admins --users=paul"
-
-oc exec -it deployment/freeipa -n ipa -- \
-    sh -c "echo Passw0rd | \
-    ipa user-add henry --first=henry \
-    --last=ipa --email=henryipa@redhatlabs.dev --password || true && \
-    ipa group-add-member ocp_devs --users=henry"
-
-oc exec -it deployment/freeipa -n ipa -- \
-    sh -c "echo Passw0rd | \
-    ipa user-add mark --first=mark \
-    --last=ipa --email=markipa@redhatlabs.dev --password || true && \
-    ipa group-add-member ocp_viewers --users=mark"
+./05-init-freeipa.sh
 
 echo -e "\tFreeIPA is ready to use!"
-
-
-# # Add Red Hat COP Helm repository and update
-# helm repo add redhat-cop https://redhat-cop.github.io/helm-charts
-# helm repo update
-
-# # Install or upgrade the IPA Helm chart
-# helm upgrade --install ipa redhat-cop/ipa --namespace=ipa --create-namespace --set app_domain=apps.${BASE_DOMAIN}
-
-# # Wait for the ipa-1-deploy pod to complete deployment
-# echo "Waiting for ipa-1-deploy pod to complete..."
-# while [[ $(kubectl get pods -n ipa -l app.kubernetes.io/name=ipa -o jsonpath='{.items[0].status.phase}') != "Running" ]]; do
-#     sleep 10
-#     echo "Still waiting..."
-# done
-
-# # Expose the IPA service as NodePort
-# oc expose service ipa --type=NodePort --name=ipa-nodeport --generator="service/v2" -n ipa
-
-# # Login to kerberos
-# oc exec -it dc/ipa -n ipa -- \
-#     sh -c "echo Passw0rd123 | /usr/bin/kinit admin && \
-#     echo Passw0rd | \
-#     ipa user-add ldap_admin --first=ldap \
-#     --last=admin --email=ldap_admin@redhatlabs.dev --password"
-    
-# # Create groups if they dont exist
-
-# oc exec -it dc/ipa -n ipa -- \
-#     sh -c "ipa group-add student --desc 'wrapper group' || true && \
-#     ipa group-add ocp_admins --desc 'admin openshift group' || true && \
-#     ipa group-add ocp_devs --desc 'edit openshift group' || true && \
-#     ipa group-add ocp_viewers --desc 'view openshift group' || true && \
-#     ipa group-add-member student --groups=ocp_admins --groups=ocp_devs --groups=ocp_viewers || true"
-
-# # Add demo users
-
-# oc exec -it dc/ipa -n ipa -- \
-#     sh -c "echo Passw0rd | \
-#     ipa user-add paul --first=paul \
-#     --last=ipa --email=paulipa@redhatlabs.dev --password || true && \
-#     ipa group-add-member ocp_admins --users=paul"
-
-# oc exec -it dc/ipa -n ipa -- \
-#     sh -c "echo Passw0rd | \
-#     ipa user-add henry --first=henry \
-#     --last=ipa --email=henryipa@redhatlabs.dev --password || true && \
-#     ipa group-add-member ocp_devs --users=henry"
-
-# oc exec -it dc/ipa -n ipa -- \
-#     sh -c "echo Passw0rd | \
-#     ipa user-add mark --first=mark \
-#     --last=ipa --email=markipa@redhatlabs.dev --password || true && \
-#     ipa group-add-member ocp_viewers --users=mark"
-
-
-
-
-
-
